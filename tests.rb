19cee7cc7334c59e59ac8f2c73ca1f0706f430df
Zloteanu Nikita <teapa011@gmail.com>
2012-11-10 10:55:08 +0200

    Removed invalid test.

--- tests.rb
+++ tests.rb
@@ -2,12 +2,5 @@ require 'test/unit'
 require_relative 'question'
 
 class Tests < Test::Unit::TestCase
-  def test_invalid_question
-    question = Question.new
-    assert_nothing_raised do
-        test = question.title + "made by "+question.author
-        puts test
-    end
-  end
 end
 
